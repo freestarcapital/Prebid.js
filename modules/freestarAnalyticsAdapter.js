dc65df6f77f880df279d8c2f3c8b9bbee7c8eca4
Anthony Greco <anthony.greco@freestar.io>
2020-01-27 12:03:58 -0500

    updates after 3.4.0 merge

---
+++ modules/freestarAnalyticsAdapter.js
@@ -0,0 +1,46 @@
+import { ajax } from 'src/ajax';
+import adapter from 'src/AnalyticsAdapter';
+import CONSTANTS from 'src/constants.json';
+import adapterManager from 'src/adapterManager';
+import { auctionManager } from 'src/auctionManager';
+import $$PREBID_GLOBAL$$ from "../";
+
+const utils = require('src/utils');
+
+const analyticsType = 'endpoint';
+
+/**
+ * Global access to auctionManager.findBidByAdId(adId);
+ * @param adId
+ * @returns {*|Object}
+ */
+$$PREBID_GLOBAL$$.findBidByAdId = function(adId) {
+  const bid = auctionManager.findBidByAdId(adId);
+  return bid;
+}
+
+let freestarAnalytics = Object.assign(adapter({ analyticsType }),
+  {
+    // Override AnalyticsAdapter functions by supplying custom methods
+    track({ eventType, args }) {
+      if (freestar.msg && freestar.msg.que) {
+        //console.log('push message:'+eventType+' to queue: '+JSON.stringify(args));
+        freestar.msg.que.push({ eventType, args });
+      }
+    }
+  });
+
+// save the base class function
+freestarAnalytics.originEnableAnalytics = freestarAnalytics.enableAnalytics;
+
+// override enableAnalytics so we can get access to the config passed in from the page
+freestarAnalytics.enableAnalytics = function (config) {
+  freestarAnalytics.originEnableAnalytics(config);  // call the base class function
+};
+
+adapterManager.registerAnalyticsAdapter({
+  adapter: freestarAnalytics,
+  code: 'freestar'
+});
+
+export default freestarAnalytics;
