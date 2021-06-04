e0a9095f0701170257c44cf35446ceb0e4e24dfb
Alex Cook <alexcook@Alexs-MacBook-Pro.local>
2021-06-04 08:25:17 -0700

    Bad merge

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -1103,15 +1103,6 @@ export const spec = {
       utils.mergeDeep(payload, {user: commonFpd.user});
     }
 
-    // First Party Data
-    const commonFpd = config.getConfig('ortb2') || {};
-    if (commonFpd.site) {
-      utils.mergeDeep(payload, {site: commonFpd.site});
-    }
-    if (commonFpd.user) {
-      utils.mergeDeep(payload, {user: commonFpd.user});
-    }
-
     // Note: Do not move this block up
     // if site object is set in Prebid config then we need to copy required fields from site into app and unset the site object
     if (typeof config.getConfig('app') === 'object') {
