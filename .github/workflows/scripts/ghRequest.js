456b883c91a2309e86228230f9356e8d0802b3f1
pankivanastasiya <anastasiya.pankiv@freestar.io>
2026-01-21 11:44:49 +0200

    Removed git actions

--- .github/workflows/scripts/ghRequest.js
+++
@@ -1,9 +0,0 @@
-module.exports = function githubRequester(github) {
-  return function (verb, params) {
-    return github.request(verb, Object.assign({
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      }
-    }, params))
-  }
-}
